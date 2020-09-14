54a9f236f39ee0ca7972729781a952e9513aeba6
Clark Bynum <cbynum@clearblade.com>
2020-09-14 14:56:15 -0500

    test - change watchPath

--- cmds/start.js
+++ cmds/start.js
@@ -57,7 +57,7 @@ let msg;
 
 const onMessagingSuccess = () => {
   console.log(chalk.green(`MQTT connected on port ${messagePort}`));
-  const watchPath = path.join('.', 'portals', portalName, 'config');
+  const watchPath = path.join('portals', portalName, 'config');
   console.log('watchPath', watchPath);
   const watcher = chokidar.watch(watchPath);
   watcher.on("change", (filepath) => {
